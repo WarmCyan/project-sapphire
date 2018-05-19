d4702e9cf2bc5c8e33d1d750e21ab43f9be95e6f
Nathan Martindale <nmblenderdude0@gmail.com>
2018-05-19 11:31:59 -0500

    Progress on reuters scraper

--- sapphire/scrapers/reuters.py
+++ sapphire/scrapers/reuters.py
@@ -1,3 +1,16 @@
+#***************************************************************************
+#
+#  File: reuters.py (sapphire.scrapers)
+#  Date created: 05/17/2018
+#  Date edited: 05/19/2018
+#
+#  Author: Nathan Martindale
+#  Copyright © 2018 Digital Warrior Labs
+#
+#  Description: Classes to handle scraping reuters RSS feeds and articles
+#
+#***************************************************************************
+
 from bs4 import BeautifulSoup
 import urllib.request
 
@@ -11,9 +24,34 @@ class RSSScraper:
     def __init__(self):
         pass
 
+    def scrape(self):
+        url = "http://feeds.reuters.com/Reuters/worldNews" # TODO: temp
+
+        # scrape the RSS from the url
+        request = urllib.request.Request(url)
+        response = urllib.request.urlopen(request)
+        page = response.read().decode('utf-8')
+
+        scrape_time_dt = datetime.datetime.now()
+        scrape_time = sapphire.utility.getTimestamp(scrape_time_dt)
+        
+        # store the scrape
+        filename = sapphire.utility.getFileTimeStamp(scrape_time_dt) + "_reuters_testscrape.xml" # TODO: or reverse order?
+        with open(sapphire.utility.feed_scrape_raw_tmp_dir + "/" + filename, 'w') as f:
+            f.write(page)
+
+    def extract(self, page):
+        pass
+
     # TODO: returns JSON
     # NOTE: make this "run" instead, and have scrape versus extract/parse separate functions?
-    def scrape(self):
+    def run(self):
+
+
+
+
+
+
 
         #print(sapphire.utility.feed_scrape_raw_tmp_dir) # TODO: debug
 
--- sapphire/utility/__init__.py
+++ sapphire/utility/__init__.py
@@ -1,3 +1,16 @@
+#***************************************************************************
+#
+#  File: __init__.py (sapphire.utility)
+#  Date created: 05/17/2018
+#  Date edited: 05/19/2018
+#
+#  Author: Nathan Martindale
+#  Copyright © 2018 Digital Warrior Labs
+#
+#  Description: Various global utility and config functions for sapphire
+#
+#***************************************************************************
+
 import datetime
 from pytz import timezone
 
@@ -6,7 +19,7 @@ import json
 from sapphire.utility.exceptions import BadSettings
 
 
-feed_scrape_raw_tmp_dir = ""
+feed_scrape_raw_dir = ""
 feed_scrape_tmp_dir = ""
 metadata_queue_dir = ""
 
@@ -16,15 +29,15 @@ metadata_store = ""
 # datetime format directives from https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
 
 def readConfig(config):
-    global feed_scrape_raw_tmp_dir
+    global feed_scrape_raw_dir
     settings = {}
     with open(config, 'r') as f:
         settings = json.load(f)
         
     try:
-        feed_scrape_raw_tmp_dir = settings["feed_scrape_raw_tmp_dir"]
+        feed_scrape_raw_dir = settings["feed_scrape_raw_dir"]
     except KeyError:
-        raise BadSettings("Setting 'feed_scrape_raw_tmp_dir' not found")
+        raise BadSettings("Setting 'feed_scrape_raw_dir' not found")
     
 
 def writeConfig():
